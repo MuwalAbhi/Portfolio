eb073f46b29931dcb3c678887a390b7cf9687957
MuwalAbhi <abhimuwal29@gmail.com>
2024-07-06 17:30:43 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Abhishek, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+        Class X 91.67%, <br />
+        Pragati Public Sr. Sec. School Kota <br />
+         Class XII 96.60% <br />
+         Saraswati Sr. Sec. School Kota
+        <br />
+        Bachelor of Technology(B.Tech),
+        2021-2025
+        <br />
+        PDPM IIITDMJ Jabalpur
+        <br />
+        Major in Computer Science & Engineering,
+        <br />
+        Relevant Courses: <br />
+             Data Structure & Algorithms, Data Science ,DataBase Management System, Computer Network, Operating System, OOPS, Next Generation Networks, Java Programming , Software Engineering , Artificial Intelligence, Cryptography.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Programming Languages: C++, C, Python , Javascript, HTML, Java <br />
+           DataBases: MongoDB, MySQL <br />
+           Libraries/Frameworks: NodeJs, ReactJs, Bootstrap, Tailwind,
+          Excellent problem-solving skills Effective communicator and
+          collaborator
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          Fusion &nbsp;&nbsp;&nbsp;&nbsp; IIITDMJ(Jan-Apr 2025) <br />
+          Software Developer	<br /> <br />
+Collaborated with a cross-functional team of 2 designers and 5 developers,<br /> working together to revamp the website design
+and create a cohesive, visually stunning interface.  <br />
+●	Reduced website bugs through testing and troubleshooting, resulting in 50% faster page loading times. <br />
+●	Integrated 5+ APIs . Implemented caching using cache manager, reducing response time by 4x. <br />
+●	Produced reports, readme, and other documentation to assist new members, resulting in a 40% reduction in training <br />
+time and increased eﬀiciency. <br /> <br />
+Tech Stack: Flutter , Dart , Django.
+
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+          Get a rank 15 in class 12 exams
+          <br />
+          3* on Codechef <br />
+          400+ problem solved on Leetcode <br />
+          Maintain a streak of 140 days on Leetcode
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative software development solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/PortFolio.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import pic from "../../public/abhi.jpg"
+
+function PortFolio() {
+ 
+  const cardItem = [
+    {
+      id: 1,
+      logo: "https://th.bing.com/th/id/OIP.qtxvbuVcVvPSy-vYZlLwoQHaHa?w=162&h=185&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+      name: "BookStore",
+      desc:"In this project I have created a web app where users can browse and read books.",
+      link:"https://github.com/MuwalAbhi/BookStore/tree/main"
+    },
+    {
+      id: 2,
+      logo: pic,
+      name: "portfolio",
+      desc:"This is my portfolio project where I have presented my projects and experience.",
+      link:"https://github.com/MuwalAbhi/Portfolio",
+    },
+    {
+      id: 3,
+      logo: "https://th.bing.com/th/id/OIP.Gumz-ykx_9V55B5Whie8rAHaHa?w=196&h=197&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+      name: "Online Voting System",
+      desc:"Voters can vote using their voting id only once when the voting window is open.",
+      link:""
+    },
+    {
+      id: 4,
+      logo: "https://th.bing.com/th?id=OIP.-wcXcPg9mUaWmMJuoWXgHgHaE8&w=306&h=204&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+      name: "RoomBooking",
+      desc:"Users can browse different rooms and can book their selected room online.",
+      link:"",
+    },
+   
+  ];
+  return (
+    <div
+      name="Portfolio"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 mt-10"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">PortFolio</h1>
+        <span className=" underline font-semibold">Featured Projects</span>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-3 my-5">
+          {cardItem.map(({ id, logo, name,desc ,link}) => (
+            <div
+              className="md:w-[300px] md:h-[300px] border-[2px] rounded-lg shadow-lg p-1 cursor-pointer hover:scale-110 duration-300"
+              key={id}
+            >
+              <img
+                src={logo}
+                className="w-[120px] h-[120px] p-1 rounded-full border-[2px]"
+                alt=""
+              />
+              <div>
+                <div className="px-2 font-bold text-xl mb-2">{name}</div>
+                <p className="px-2 text-gray-700">
+                 {desc}
+                </p>
+              </div>
+              <div className=" px-6 py-4 space-x-3 justify-around">
+              <a href={link} target="_blank" rel="noopener noreferrer">
+              <button className="bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 rounded"  >
+                  Source code
+                </button>
+      </a>
+               
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PortFolio;
